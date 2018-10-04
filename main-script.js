3dc33a1effc250f3293749a7e34b83e8f468a756
n6600shaw <shawliu0315@gmail.com>
2018-10-03 21:34:46 -0230

    add list

--- main-script.js
+++ main-script.js
@@ -1,7 +1,26 @@
  $(document).ready(function(){
-     alert("clicked");
+     
      $( function() {
        $( "#job-from-1" ).datepicker({ dateFormat: 'dd/mm/yy' });
        $( "#job-to-1" ).datepicker({ dateFormat: 'dd/mm/yy' });
      });
+
+     $("#create").click(function(){
+
+
+      var table = $("#project-list");
+
+ 
+      var title = $("#project-title").val();
+      var supervisor=$("#project-supervisor").val();
+      
+      table.append('<tr><th>'+title+'</th><th>'+supervisor+'</th><th><button type="button" class="close" aria-label="Close" <span aria-hidden="true">&times;</span> </button></th></tr>');
+      
+
+
+     })
+
+     
+
+
    });    
