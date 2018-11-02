53805a218e8300a78a491f8b3af63f00dec360dc
n6600shaw <shawliu0315@gmail.com>
2018-11-01 23:09:23 -0230

    assignment2

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var app = express()
-var port=12804
-
+var port=3333
+var path=require('path');
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('hello world')
+
+   res.sendFile(path.join(__dirname+'/create-project.html'));
 })
 
-app.listen(port,()=>console.log('example app listening on port ${port}!'))
+app.listen(port,()=>console.log('GradRec is listening on port ${port}!'))
